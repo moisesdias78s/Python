9279c852ae32a3bdf9da1f5eab655fa1eb7b4ab0
moisesdias78s <moisesdias784s@gmail.com>
2026-07-07 17:53:54 -0300

    Eu iniciei um projeto de um interpretador

---
+++ Phyton/Interpretador_comandos.py
@@ -0,0 +1,23 @@
+#===================================
+#Interpretador python 3.14
+#===================================
+
+#Para abrir o interpretador python é necessario digitar no terminal (python3.14) ou no windows tambem tem a opação do py.
+
+#Para digitar um caracter de fim de arquivo digite, quit().
+
+'''
+python -c "print(a+b)" #executa o codigo escrito diretamente na linha de comando.
+python -m pip #executa um modulo python atraves de uma linha de comando.
+python -i executa o programa e depois abre um menu interativo.
+
+'''
+
+
+#py -c "a=2; b=3;  print(a+b) " é utilizado o (;) para saber onde inicia uma instrução e onde finaliza a instrução.
+
+a = 2
+
+b = 3
+
+print(a+b)
